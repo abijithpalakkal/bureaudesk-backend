8d98e527dcab4315e9b644f59b9617ac8adc38d0
Abijith P <abhijithunni00@gmail.com>
2024-07-27 09:52:54 +0530

    added redis serivce

--- auth/src/utils/redisClient.ts
+++ auth/src/utils/redisClient.ts
@@ -1,6 +1,15 @@
-import { Redis } from "ioredis";
+import { Redis,RedisOptions } from "ioredis";
 
-export const Client = new Redis();
+
+const redisHost = process.env.REDIS_HOST || "redis-stack";
+const redisPort = parseInt(process.env.REDIS_PORT || "6379", 10);
+
+const redisConfig: RedisOptions = {
+  host: redisHost,
+  port: redisPort,
+};
+
+export const Client = new Redis(redisConfig);
 
 Client.on("connect",() => {
     
