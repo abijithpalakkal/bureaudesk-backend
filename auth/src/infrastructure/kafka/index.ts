91d70929014d6375976181091824afe7a781e0f3
Abijith P <abhijithunni00@gmail.com>
2024-08-30 16:07:55 +0530

    added confluent in all services

--- auth/src/infrastructure/kafka/index.ts
+++ auth/src/infrastructure/kafka/index.ts
@@ -1,9 +1,23 @@
 import { Kafka,Producer,Consumer } from "kafkajs";
 
-const kafka = new Kafka({
-    clientId: "auth-service",
-    brokers:["kafka:9092"]
-})
+// const kafka = new Kafka({
+//     clientId: "auth-service",
+//     brokers:["kafka:9092"]
+// })
+
+export const kafka = new Kafka({
+    clientId: 'auth-service',
+    brokers: ['pkc-4j8dq.southeastasia.azure.confluent.cloud:9092'],
+    ssl: true,
+    sasl: {
+      mechanism: 'plain',
+      username: 'IGI4TMEEZDD5XDZG',
+      password:'KYygx3UkksOGC9+Iur1t5EPU3MlyQfY2qBgJ1zHfxW3leYtYefDoikTYcR8EjsPk',
+    },
+    connectionTimeout: 30000, 
+      authenticationTimeout: 30000, 
+  
+  });
 
 export const producer: Producer = kafka.producer();
 export const consumer: Consumer = kafka.consumer({
--- notification/src/kafka/kafka.service.ts
+++ notification/src/kafka/kafka.service.ts
@@ -5,9 +5,23 @@ import { NotificationService } from 'src/notification/notification.service';
 
 @Injectable()
 export class KafkaService implements OnModuleInit {
+    // private readonly kafka = new Kafka({
+    //     brokers: ['kafka:9092'], // Replace with your Kafka broker address
+    // });
+
     private readonly kafka = new Kafka({
-        brokers: ['kafka:9092'], // Replace with your Kafka broker address
-    });
+        clientId: 'notification-service',
+        brokers: ['pkc-4j8dq.southeastasia.azure.confluent.cloud:9092'],
+        ssl: true,
+        sasl: {
+          mechanism: 'plain',
+          username: 'IGI4TMEEZDD5XDZG',
+          password:
+            'KYygx3UkksOGC9+Iur1t5EPU3MlyQfY2qBgJ1zHfxW3leYtYefDoikTYcR8EjsPk',
+        },
+        connectionTimeout: 30000,
+        authenticationTimeout: 30000,
+      });
 
     private readonly consumer: Consumer = this.kafka.consumer({ groupId: 'my-consumer-group' });
     constructor(private readonly notificationService: NotificationService) { }
