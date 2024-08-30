7816dfa8780d08b9bbf261b30e9ae82ec10eeef1
Abijith P <abhijithunni00@gmail.com>
2024-08-30 17:03:08 +0530

    added dockerfile to api-gateway

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -14,11 +14,11 @@ app.use(cors(corsOptions));
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/auth',proxy('http://localhost:3000/'));
-app.use('/user',proxy('http://localhost:3001/'));
-app.use('/company',proxy('http://localhost:3002/'));
-app.use('/chat',proxy('http://localhost:3005/'));
-app.use('/notification',proxy('http://localhost:3007'));
+app.use('/auth',proxy('https://bureaudesk-backend-auth.onrender.com/'));
+app.use('/user',proxy('https://bureaudesk-backend-user.onrender.com/'));
+app.use('/company',proxy('https://bureaudesk-backend-company.onrender.com/'));
+app.use('/chat',proxy('https://bureaudesk-backend-chat.onrender.com/'));
+app.use('/notification',proxy('https://bureaudesk-backend.onrender.com/'));
 
 
 app.listen(port,()=>{
