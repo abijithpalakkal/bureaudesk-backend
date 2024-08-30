7419ca91b071563f5ffc796b135dfb66caaa088d
Abijith P <abhijithunni00@gmail.com>
2024-08-30 19:52:15 +0530

    changed cors error

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -6,7 +6,8 @@ import proxy from "express-http-proxy"
 const app:Application = express()
 const port : number = 8000
 const corsOptions={
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'https://bureaudesk-frontend.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }
